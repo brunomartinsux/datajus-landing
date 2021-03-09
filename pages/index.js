5c8a3af73226dacb3fddb1e4f8c299468ce49417
Bruno Luis Martins <zapelim.dev>
2021-03-09 10:54:00 -0300

    form-sub

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import Form from '../components/Form'
 import Infodata from '../components/Infodata'
 import Products from '../components/Products'
 import TitleSubtitle from '../components/TitleSubtitle'
+import Head from 'next/head'
 
 
 export default function Home() {
   return (
     <div>
+    <Head>
+        <title>Datajus</title>
+    </Head>
     <Banner />
     <Products />
     <TitleSubtitle/>
